bf2ff292919bc4590f67af150b01f3c9d9dec760
Themon645 <hilmanh707@gmail.com>
2024-03-01 21:09:46 +0700

    tugas 3

--- tugas/tugas3/3a.php
+++ tugas/tugas3/3a.php
@@ -4,7 +4,7 @@ function hitungluaslingkaran($r) {
     $luas = 3.14 * $r * $r;
     return $luas;
 }
-echo "jari-jari = 10 cm. <br> Luas Lingkaran = " . hitungluaslingkaran(10);
+echo "jari-jari = 10 cm. <br> Luas Lingkaran = " . hitungluaslingkaran(10) . " cm<sup>2</sup>";
 echo "<hr>";
 
 
@@ -14,7 +14,7 @@ function hitungkelilinglingkaran($r) {
     $keliling = 3.14 * $r * 2;
     return $keliling; 
 }
-echo "jari-jari = 20 cm. <br> Keliling Lingkaran = " . hitungkelilinglingkaran(20);
+echo "jari-jari = 20 cm. <br> Keliling Lingkaran = " . hitungkelilinglingkaran(20) . " cm";
 echo "<hr>";
 
 ?>
--- tugas/tugas3/3b.php
+++ tugas/tugas3/3b.php
@@ -1,8 +1,9 @@
 <?php
 function urutanAngka ($angka) {
+  $jumlah = 1;
   for ($i = 1; $i <= $angka; $i++) {
     for ($j = 1; $j <= $i; $j++){
-      echo "$j";
+      echo $jumlah++ . " ";
     }
     echo "<br>";
   }
